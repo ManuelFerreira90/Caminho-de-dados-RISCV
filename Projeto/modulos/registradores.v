935897c3e8a99abef2785ad187c22f440ea37aeb
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2023-05-30 23:18:36 -0300

    Update registradores.v

--- Projeto/modulos/registradores.v
+++ Projeto/modulos/registradores.v
@@ -16,6 +16,38 @@ module registradores (clk, rs1, rs2, rd, readdata1R, readdata2R, regiwrite, memt
 
     initial begin
         $readmemb("entrada/registradores.bin", bancoregistradores); // Lendo registradores
+        reg0 <= bancoregistradores[0];
+        reg1 <= bancoregistradores[1];
+        reg2 <= bancoregistradores[2];
+        reg3 <= bancoregistradores[3];
+        reg4 <= bancoregistradores[4];
+        reg5 <= bancoregistradores[5];
+        reg6 <= bancoregistradores[6];
+        reg7 <= bancoregistradores[7];
+        reg8 <= bancoregistradores[8];
+        reg9 <= bancoregistradores[9];
+        reg10 <= bancoregistradores[10];
+        reg11 <= bancoregistradores[11];
+        reg12 <= bancoregistradores[12];
+        reg13 <= bancoregistradores[13];
+        reg14 <= bancoregistradores[14];
+        reg15 <= bancoregistradores[15];
+        reg16 <= bancoregistradores[16];
+        reg17 <= bancoregistradores[17];
+        reg18 <= bancoregistradores[18];
+        reg19 <= bancoregistradores[19];
+        reg20 <= bancoregistradores[20];
+        reg21 <= bancoregistradores[21];
+        reg22 <= bancoregistradores[22];
+        reg23 <= bancoregistradores[23];
+        reg24 <= bancoregistradores[24];
+        reg25 <= bancoregistradores[25];
+        reg26 <= bancoregistradores[26];
+        reg27 <= bancoregistradores[27];
+        reg28 <= bancoregistradores[28];
+        reg29 <= bancoregistradores[29];
+        reg30 <= bancoregistradores[30];
+        reg31 <= bancoregistradores[31];
     end
 
     always @(posedge clk) begin
