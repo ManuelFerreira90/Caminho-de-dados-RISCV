2a93141eb381dcad557fa66868594fcee773ae7c
Manuel Ferreira <105729881+ManuelFerreira90@users.noreply.github.com>
2023-06-14 16:15:16 -0300

    testbench para FPGA

---
+++ testbench-FPGA/conversaodisplay.v
@@ -0,0 +1,14 @@
+module conversaodisplay (dezenapc, unidadepc, dezenareg, unidadereg, pc, register);
+	input [31:0] pc;
+	input [31:0] register;
+	output [3:0] dezenapc;
+	output [3:0] unidadepc;
+	output [3:0] dezenareg;
+	output [3:0] unidadereg;
+	
+	assign dezenapc = pc / 10;
+	assign unidadepc = pc % 10;
+	assign dezenareg = register / 10;
+	assign unidadereg = register % 10;
+	
+endmodule
